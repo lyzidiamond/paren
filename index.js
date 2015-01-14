216dd2a695c8051dfbe0d49a9faa6ea8d72f6624
Tom MacWright <tom@macwright.org>
2015-01-14 15:34:25 -0500

    Unify into index.js, use es6 class methods for everything

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+/* global L */
 var debounce = require('debounce'),
-  createWidget = require('./create_widget'),
   CodeMirror = require('codemirror'),
   Terrarium = require('terrarium').Browser;
-
 require('codemirror/mode/javascript/javascript');
 
-var pairs = (o) => Object.keys(o).map(k => [k, o[k]]);
-var values = (o) => pairs(o).map(k => k[1]);
+require('mapbox.js');
+var geojsonhint = require('geojsonhint').hint,
+  highlight = require('highlight.js');
 
-function $(_) { return document.getElementById(_); }
+
+var pairs = (o) => Object.keys(o).map(k => [k, o[k]]);
+
 function ce(_, c) {
   var elem = document.createElement(_);
   elem.className = c || '';
@@ -22,59 +24,67 @@ function ce(_, c) {
  */
 class Rpl {
   constructor(element, options) {
-    var widgets = [], errors = [], delayedClear, terrarium;
+    this.element = element;
+    this.options = options || {};
+    this.options.mapid = this.options.mapid || 'tmcw.map-7s15q36b';
+    this.options.accessToken = this.options.accessToken || 'pk.eyJ1IjoidG1jdyIsImEiOiJIZmRUQjRBIn0.lRARalfaGHnPdRcc-7QZYQ';
+    this.widgets = [];
+    this.errors = [];
+    this.delayedClear = null;
+    this.terrarium = null;
+    this.editor = this.setupEditor(this.element);
+    this.editor.on('change', debounce(this.onchange.bind(this), 200));
+    this.onchange();
+  }
 
-    var editor = this.setupEditor(element);
-    editor.on('change', debounce(onchange, 200));
-    onchange();
+  onchange() {
+    clearTimeout(this.delayedClear);
+    this.joinWidgets({});
+    if (this.terrarium) { this.terrarium.destroy(); }
+    this.terrarium = new Terrarium(this.options);
+    this.terrarium
+      .on('data', this.ondata.bind(this))
+      .on('err', this.onerr.bind(this))
+      .run(this.editor.getValue());
+  }
 
-    function onchange() {
-      clearTimeout(delayedClear);
-      joinWidgets([]);
-      if (terrarium) { terrarium.destroy(); }
-      terrarium = new Terrarium(options);
-      terrarium
-        .on('data', ondata)
-        .on('err', onerr)
-        .run(editor.getValue());
+  onerr(err) {
+    this.clearErrors();
+    if (err.message) {
+      var elem = ce('div', 'rpl-error');
+      elem.innerHTML = err.toString();
+      this.errors.push(this.editor.addLineWidget(err.lineNumber || 0,
+        elem, { coverGutter: false, noHScroll: true }));
     }
+  }
 
-    function ondata(d) {
-      clearTimeout(delayedClear);
-      joinWidgets(d);
-    }
+  ondata(d) {
+    clearTimeout(this.delayedClear);
+    this.joinWidgets(d);
+  }
 
-    function onerr(err) {
-      errors.forEach(e => editor.removeLineWidget(e));
-      if (err.message) {
-        var elem = document.createElement('div');
-        elem.innerHTML = err.toString();
-        elem.className = 'rpl-error';
-        var widget = editor.addLineWidget(
-          err.lineNumber || 0,
-          elem, { coverGutter: false, noHScroll: true });
-          errors.push(widget);
-      }
-    }
+  clearErrors() {
+    this.errors.forEach(this.editor.removeLineWidget);
+    this.errors = [];
+  }
 
-    function joinWidgets(newData) {
-      newData = newData || {};
-      // remove old widgets
-      widgets.forEach(widget => editor.removeLineWidget(widget));
-      errors.forEach(e => editor.removeLineWidget(e));
-      widgets = pairs(newData).map(p => addWidget(p[1], p[0]));
+  clearWidgets() {
+    this.widgets.forEach(this.editor.removeLineWidget);
+    this.widgets = [];
+  }
 
-      function addWidget(val, id) {
-        var line = val[val.length - 1].line - 1;
-        var el = makeWidget(val);
-        var widget = editor.addLineWidget(
-          line, el, { coverGutter: false, noHScroll: true });
-        if (el.widget.onadd) {
-          el.widget.onadd();
-        }
-        return widget;
-      }
-    }
+  joinWidgets(newData) {
+    this.clearWidgets();
+    this.clearErrors();
+    this.widgets = pairs(newData || {}).map(p => {
+      var [id, val] = p;
+      var line = val[val.length - 1].line - 1;
+      var el = this.makeWidget(val);
+      var widget = this.editor.addLineWidget(
+        line, el, { coverGutter: false, noHScroll: true });
+      if (el.widget.onadd) el.widget.onadd();
+      return widget;
+    });
   }
 
   setupEditor(element) {
@@ -85,21 +95,44 @@ class Rpl {
       viewportMargin: Infinity
     });
   }
-}
 
-function makeWidget(values) {
-  var value = values[values.length - 1],
-    msg = ce('div'),
-    div = msg.appendChild(ce('div')),
-    n = msg.appendChild(ce('div', 'data-name')),
-    name = n.appendChild(ce('span', 'data-var'));
-  n.className = 'data-name';
-  name.innerHTML = value.name;
-  msg.className = 'data';
-  try {
-    msg.widget = createWidget(div, value.val);
-  } catch(e) { console.error(e); }
-  return msg;
+  makeWidget(values) {
+    var value = values[values.length - 1],
+      msg = ce('div'),
+      div = msg.appendChild(ce('div')),
+      n = msg.appendChild(ce('div', 'data-name')),
+      name = n.appendChild(ce('span', 'data-var'));
+    n.className = 'data-name';
+    name.innerHTML = value.name;
+    msg.className = 'data';
+    try {
+      msg.widget = this.fillWidget(div, value.val);
+    } catch(e) { console.error(e); }
+    return msg;
+  }
+
+  fillWidget(container, value) {
+    L.mapbox.accessToken = this.options.accessToken;
+
+    if (value && !geojsonhint(JSON.stringify(value)).length) {
+      var element = container.appendChild(ce('div', 'map-viewer')),
+        featureLayer = L.mapbox.featureLayer(value),
+        map = L.mapbox.map(element, this.options.mapid, {
+        zoomControl: false, maxZoom: 15, scrollWheelZoom: false
+      }).addLayer(featureLayer);
+      element.onadd = function() {
+        map.fitBounds(featureLayer.getBounds());
+        map.invalidateSize();
+      };
+      return element;
+    } else {
+      var pre = container.appendChild(ce('pre', 'json-viewer'));
+      pre.innerHTML = JSON.stringify(value, null, 2);
+      highlight.highlightBlock(pre);
+      pre.onadd = function() { };
+      return pre;
+    }
+  }
 }
 
 module.exports = Rpl;
